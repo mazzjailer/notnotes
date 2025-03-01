45f48874eb91acee946bf2c6f849da51592c61c6
mazzjailer <mzyramini@gmail.com>
2025-03-01 03:59:00 +0330

    add Table component and implement it in notes page

--- src/app/card.jsx
+++ src/app/card.jsx
@@ -2,19 +2,20 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import './globals.css'
+import { TableCell, TableRow } from '@/components/ui/table'
 
 const Card = (props) => {
   const router = useRouter();
   return (
-    <tr onClick={() => router.push(`/notes/${props.push}`)} className='container w-full border-solid border-t border-b border-neutral-200 hover:cursor-pointer hover:bg-neutral-100 dark:hover:bg-neutral-900 '>
-      <td className='pr-6 pl-6 pt-2 pb-2'>
+    <TableRow onClick={() => router.push(`/notes/${props.push}`)} className='hover:cursor-pointer'>
+      <TableCell className=''>
         <h4 className='text-xl text-black dark:text-white font-medium'>{props.title}</h4>
         <p className='text-lg text-neutral-800 dark:text-neutral-400'>{props.abstract}</p>
-      </td>
-      <td className='pr-6 pl-6 pt-2 pb-2'>
+      </TableCell>
+      <TableCell className='text-base'>
         <h4>📅&nbsp;{new Date (props.date).toLocaleString("en-GB")}</h4>
-      </td>
-    </tr>
+      </TableCell>
+    </TableRow>
   )
 }
 
--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -6,6 +6,7 @@ import { PrismaClient } from '@prisma/client'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 export default async function App( {searchParams} ) {
   const { sortOrder = '' } = await searchParams;
@@ -41,23 +42,23 @@ export default async function App( {searchParams} ) {
       <div className='flex flex-col'>
         <div className='flex flex-row items-center justify-center'>
         </div>
-          <table className='w-full'>
-            <thead className='text-left text-neutral-600 dark:text-neutral-300 text-lg'>
-              <tr>
-                <th>
-                  <Link className='p-5' href={"/notes?search=" + search + "&sortOrder=title"}>Title</Link>
-                </th>
-                <th>
-                  <Link className='p-5' href={"/notes?search=" + search + "&sortOrder=date"}>Date</Link>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-                {notes.map((note) => (
+        <Table>
+          <TableHeader>
+            <TableRow className='hover:bg-white dark:hover:bg-black text-base'>
+              <TableHead>
+                <Link className='p-5' href={"/notes?search=" + search + "&sortOrder=title"}>Title</Link>
+              </TableHead>
+              <TableHead>
+                <Link className='p-5' href={"/notes?search=" + search + "&sortOrder=date"}>Date</Link>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+          {notes.map((note) => (
                   <Card push={note.id} key={note.id} id={note.id} title={note.title} abstract={note.content.substring(0, 50) + "..."} date={note.date} />
                 ))}
-            </tbody>
-          </table>
+          </TableBody>
+        </Table>
       </div>
       <Link href="/write" className='fixed rounded-2xl bottom-10 right-10 md:bottom-32 md:right-44 w-[60px] h-[60px] bg-black dark:bg-white z-2 text-white dark:text-black hover:cursor-pointer flex justify-center items-center text-xl'><FaPen /></Link>
     </div>
