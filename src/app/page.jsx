c928c6e5686fbb454a53b9a7e9483e1c80c5b267
mazzjailer <mzyramini@gmail.com>
2025-03-22 00:07:04 +0330

    migrate database from MySQL to PostgreSQL; update schema and remove old migrations

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ const InitialHomePage = () => {
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-center items-center gap-4'>
         <div className='col-span-1 lg:col-span-1 xl:col-span-2 flex flex-col gap-2'>
           <h1 className='text-4xl font-bold border-b'>Welcome to notNotes! 📔</h1>
-          <h2 className='text-md'>A simple note-taking app. Made with Next.js, React, TailwindCSS, Shadcn UI, Better-Auth, MySQL and Prisma ORM.</h2>
+          <h2 className='text-md'>A simple note-taking app. Made with Next.js, React, TailwindCSS, Shadcn UI, Better-Auth, PostgreSQL and Prisma ORM.</h2>
         </div>
         <SignIn />
       </div>
