8d94799f2401481df28c994422d21f4b26feaa65
mazzjailer <mzyramini@gmail.com>
2025-04-23 00:44:28 +0330

    update styles for Loading, NotePage, WritePage, and Header components for improved UI consistency

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ const Header = async () => {
   })
 
   return (
-    <div className='flex flex-row container max-w-full items-center justify-center align-middle fixed z-50 bg-white shadow-[inset_0px_-1px_10px_rgba(0,0,0,0.1)] dark:bg-black dark:shadow-[inset_0px_-7px_10px_rgba(255,255,255,0.1)]'>
+    <div className='flex flex-row container max-w-full items-center justify-center align-middle fixed z-50 bg-white shadow-[inset_0px_-1px_10px_rgba(0,0,0,0.09)] dark:bg-black dark:shadow-[inset_0px_-7px_10px_rgba(255,255,255,0.09)]'>
       <div className='flex flex-row items-center justify-between w-full p-4 pl-6 pr-6 md:pr-36 md:pl-36 border-b-[1px]'>
         <HeaderTitleAndSearchBar />
         <BackButton />
